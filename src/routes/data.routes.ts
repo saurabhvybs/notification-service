e23728b8186422078fb34bfe5cb5c4842029c1ae
saurabhvybs <psaurabh51@gmail.com>
2025-03-07 15:33:46 +0530

    kafka setup

--- src/routes/data.routes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/data.routes.ts
-import { Hono } from 'hono';
-import { dataController } from '../controllers/data.controllers';
-
-const router = new Hono();
-
-router.get('/data', dataController.getData);
-
-export default router;
--- src/routes/message.routes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/message.routes.ts
-import { Hono } from 'hono';
-import { messageController } from '../controllers/messages.controllers';
-
-const router = new Hono();
-
-router.post('/messages', messageController.sendMessage);
-
-export default router;
